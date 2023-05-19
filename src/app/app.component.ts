a65c48f808ba40ad9fe707efc8101be70dd127c0
Mateusz Aniolek <maniolek@pgs-soft.com>
2023-05-19 10:49:33 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fancy-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'fancy-app';
+}
